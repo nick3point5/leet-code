94db8cb1ce616e86c739aff2414a6e2c4a26fa7c
kennytrinh <kenny.trinh4444@gmail.com>
2021-08-08 12:21:15 -0500

    5 solved

---
+++ 1-10/5_longest_palindrome_substring.js
@@ -0,0 +1,52 @@
+var longestPalindrome = function (s) {
+  if (s.length === 1) {
+    return s
+  }
+
+  const array = s.split("")
+  let longest = ""
+
+  const letters = {}
+
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i]
+    if (!letters[letter]) {
+      letters[letter] = []
+    } else {
+
+      for (let j = 0; j < letters[letter].length; j++) {
+        if (longest.length <= i - letters[letter][j]) {
+          const substring = s.slice(letters[letter][j], i + 1)
+          if (isPalindrome(substring)) {
+            longest = substring
+          }
+        }
+
+      }
+
+    }
+    letters[letter].push(i)
+  }
+
+  if (!longest) {
+    return s[0]
+  }
+
+  return longest
+};
+
+function isPalindrome(s) {
+  let ans = true;
+  s = [...s + '']
+
+  let half = Math.floor(s.length / 2);
+  for (let index = 0; index < half; index++) {
+    if (s[index] !== s[s.length - index - 1]) {
+      ans = false;
+    }
+
+  }
+  return ans
+}
+
+console.log(longestPalindrome("zeusnilemacaronimaisanitratetartinasiaminoracamelinsuez"))
