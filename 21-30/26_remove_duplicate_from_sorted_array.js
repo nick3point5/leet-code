c657a37e4d4db8a1579c9c7556de404212e2e5ea
kennytrinh <kenny.trinh4444@gmail.com>
2021-08-10 00:01:15 -0500

    26 optimized

--- 21-30/26_remove_duplicate_from_sorted_array.js
+++ 21-30/26_remove_duplicate_from_sorted_array.js
@@ -1,15 +1,14 @@
-var removeDuplicates = function(nums) {
-  const length = nums.length
-  let lastElement = nums[0]
-  for (let i = 1, j = 1; i < length; i++) {
-    if (lastElement === nums[j]) {
-      nums.splice(j, 1)
-    } else {
-      lastElement = nums[j]
+var removeDuplicates = function (nums) {
+  if(nums.length === 0) return 0
+  if (nums.length === 1) return 1
+  let j = 0
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[j]) {
+      nums[j+1] = nums[i]
       j++
     }
   }
-  return nums.length
+  return ++j
 };
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
