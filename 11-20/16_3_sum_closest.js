2b6df1071f1d803e647749886553432a9bb74f95
kennytrinh <kenny.trinh4444@gmail.com>
2021-08-12 05:51:45 -0500

    16 solved

--- 11-20/16_3_sum_closest.js
+++ 11-20/16_3_sum_closest.js
@@ -1,37 +1,34 @@
-var threeSumClosest = function(nums, target) {
+var threeSumClosest = function (nums, target) {
   nums.sort((a, b) => a - b)
 
   let result = 0
   let minimum = Infinity
 
   for (let i = 0; i < nums.length - 2; i++) {
-    if (nums[i] != nums[i - 1]) {
-      let left = i + 1
-      let right = nums.length - 1
-      while (left < right) {
-        const sum = nums[i] + nums[left] + nums[right]
-        const difference = target-sum
-        if (difference**2 < minimum**2  ) {
-          result=sum;
-          if (sum===target) {
-            i = nums.length
-            break
-          }
-          minimum=difference;
-          left++
-          right--
-          while (nums[left] === nums[left - 1]) left++
-          while (nums[right] === nums[right + 1]) right--
-        }
-        else if (difference > 0) { right-- }
-        else if (difference < 0) { left++ }
+    let left = i + 1
+    let right = nums.length - 1
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right]
+
+      if (sum === target) return sum
+
+      const difference = target-sum
+
+      if (difference ** 2 < minimum ** 2) {
+        minimum = difference
+        result = sum
       }
+      sum > target ? right-- : left++
     }
   }
+
   return result
 };
 
-console.log(threeSumClosest([0,2,1,-3],1))
+const nums = [4, 0, 5, -5, 3, 3, 0, -4, -5], target = -1
+
+console.log(threeSumClosest(nums, target))
 
 
 // https://leetcode.com/problems/3sum-closest/
