a2fef6cfb25dc97b044720f53836aa429fdd64c6
kennytrinh <kenny.trinh4444@gmail.com>
2021-08-13 15:05:27 -0500

    28 optimized

--- 21-30/28_implement_strStr.js
+++ 21-30/28_implement_strStr.js
@@ -1,16 +1,5 @@
 var strStr = function(haystack, needle) {
-    if (haystack === needle) {
-      return 0
-    }
-
-    regex = new RegExp(needle+"[a-z]*", "g")
-    leftStack = haystack.replace(regex,"")
-
-    if (haystack === leftStack) {
-      return -1
-    }
-
-    return leftStack.length
+  return (haystack.indexOf(needle));
 };
 
 // https://leetcode.com/problems/implement-strstr/
